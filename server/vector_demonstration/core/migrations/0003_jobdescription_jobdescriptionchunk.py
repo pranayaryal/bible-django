292c86d5321792aab43de4493df429e13ba58413
pranayaryal <drpranayaryal@gmail.com>
2024-05-31 23:17:03 -0400

    initial commit

--- server/vector_demonstration/core/migrations/0003_jobdescription_jobdescriptionchunk.py
+++ server/vector_demonstration/core/migrations/0003_jobdescription_jobdescriptionchunk.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='JobDescription',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
+                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)),
                 ('datetime_created', models.DateTimeField(auto_now_add=True)),
                 ('last_edited', models.DateTimeField(auto_now=True)),
                 ('title', models.CharField(max_length=255)),
@@ -33,7 +33,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='JobDescriptionChunk',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
+                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)),
                 ('datetime_created', models.DateTimeField(auto_now_add=True)),
                 ('last_edited', models.DateTimeField(auto_now=True)),
                 ('chunk', models.TextField()),
--- server/vector_demonstration/core/models.py
+++ server/vector_demonstration/core/models.py
@@ -1,10 +1,12 @@
 import csv
 import glob
+import uuid
 import html
 import logging
 import os
 import re
 import time
+import re, html
 
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser, BaseUserManager
@@ -17,6 +19,8 @@ from sentence_transformers import SentenceTransformer
 from transformers import AutoTokenizer
 from vector_demonstration.common.models import AbstractBaseModel
 from vector_demonstration.utils.sites import get_site_url
+from pgvector.django import VectorField
+
 
 logger = logging.getLogger(__name__)
 
@@ -87,6 +91,8 @@ class User(AbstractUser, AbstractBaseModel):
 
 
 class JobDescription(AbstractBaseModel):
+    """A job description"""
+
     title = models.CharField(max_length=255)
     company = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
@@ -96,61 +102,64 @@ class JobDescription(AbstractBaseModel):
 
     def __str__(self):
         return self.title
-
+    
     @classmethod
     def import_job_descriptions(cls):
+        # psql postgresql://vector_demonstration:password@127.0.0.1:56432/vector_demonstration_db
+        # python manage.py shell_plus
+
         def get_jobs_csv():
-            """Find all the CSV files in the jobs directory and return them as a generator"""
-            data_directory = os.path.join(settings.BASE_DIR, "..", "..", "data", "jobs")
+            data_directory = os.path.join(settings.BASE_DIR, '..', 'data', 'jobs')
             csv_paths = glob.glob(f"{data_directory}/*.csv")
             for csv_path in csv_paths:
-                with open(csv_path, "r") as f:
+                with open(csv_path, 'r') as f:
                     yield f
 
         for csvfile in get_jobs_csv():
-            print(f"Loading {csvfile.name}...")
+            print(f'Loading {csvfile.name}...')        
             start_time = time.time()
+            
 
-            csvreader = csv.DictReader(csvfile, delimiter=",")
-
+            csvreader = csv.DictReader(csvfile, delimiter=',')
             job_descriptions = []
             for row in csvreader:
                 # try:
-                #     language = detect(row["description"])
+                #     language = detect(row['description'])
                 # except LangDetectException:
-                #     language = ""
+                #     language = ''
+                
                 job_descriptions.append(
                     cls(
-                        title=row["title"],
-                        company=row["company"],
-                        location=row["location"],
-                        description=row["description"],
-                        skills=row["skills"],
-                        # language=language,
+                        title=row['title'],
+                        company=row['company'],
+                        location=row['location'],
+                        description=row['description'],
+                        skills=row["skills"]
+                        # language=language
                     )
                 )
 
             cls.objects.bulk_create(job_descriptions)
-
-            print(f"    Loaded in {time.time() - start_time} seconds.")
-
+            print(f'Loaded in {time.time() - start_time} seconds.')
+    
     @classmethod
     def detect_languages(cls):
         def get_job_descriptions():
             page_size = 1000
             num_pages = cls.objects.count() // page_size + 1
             for page in range(num_pages):
-                qs = cls.objects.filter(language="")[page * page_size : (page + 1) * page_size]
+                qs = cls.objects.all()[page * page_size : (page + 1) * page_size]
                 for job_description in qs:
                     yield job_description
-
+        
         count = 0
-        total_jds = cls.objects.filter(language="").count()
+        total_jds = cls.objects.count()
+        
         for job_description in get_job_descriptions():
             count += 1
-            print(f"Detecting language for JD #{count} of {total_jds}...")
+            print(f'Detecting language for JD # {count} of {total_jds}')
             try:
-                language = detect(job_description.description)
+                language = detect(job_description.description) 
             except LangDetectException:
                 language = ""
             job_description.language = language
@@ -161,11 +170,11 @@ class JobDescription(AbstractBaseModel):
             tag_re = re.compile(r"(<!--.*?-->|<[^>]*>)")
             no_tags = tag_re.sub("", text)
             return html.escape(no_tags)
-
+       
+        
         def get_chunks(jd_content, chunk_size=750):
-            """Naive chunking of job description.
-
-            `chunk_size` is the number of characters per chunk.
+            """Naive chunking of job description
+            `chunk_size` is the number of characters per chunk
             """
             chunk_size = chunk_size
             content = strip_html_tags(jd_content).replace("\n", " ")
@@ -173,14 +182,16 @@ class JobDescription(AbstractBaseModel):
                 chunk, content = content[:chunk_size], content[chunk_size:]
                 yield chunk
 
-        # 1. Set up the embedding model
+        # Set up the embedding model
         model = SentenceTransformer("all-MiniLM-L6-v2")
         tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
 
-        # 2. Chunk the job description into sentences
+        
+        # Chunk the job description into sentences
+        # jd = JobDescription.objects.first()
         chunk_embeddings = ((c, tokenizer.tokenize(c), model.encode(c)) for c in get_chunks(self.description))
-
-        # 3. Save the embeddings information for each chunk
+        
+        # Save the embedding information in each chunk
         jd_chunks = []
         for chunk_content, chunk_tokens, chunk_embedding in chunk_embeddings:
             jd_chunks.append(
@@ -188,41 +199,22 @@ class JobDescription(AbstractBaseModel):
                     job_description=self,
                     chunk=chunk_content,
                     token_count=len(chunk_tokens),
-                    embedding=chunk_embedding,
+                    embedding=chunk_embedding
                 )
             )
-
+        
         JobDescriptionChunk.objects.bulk_create(jd_chunks)
-
+    
     @classmethod
     def search(cls, query=None):
         query = (
             query
-            or "The student would prefer a job in the arts. They have a background in choir and theater. Major: Music. Minor: Theater. Graduating Year: 2022"
+            or "The student would prefer a job in the arts. They have a background in choir and theater"
         )
-        # > expected result: list of Job Descriptions in descending order of relevance
-        model = SentenceTransformer("all-MiniLM-L6-v2")
-        query_embedding = model.encode(query)
 
-        jd_chunks = JobDescriptionChunk.objects.annotate(distance=L2Distance("embedding", query_embedding)).order_by("distance")
+            
 
-        unique_jds = {}
-        for chunk in jd_chunks:
-            if chunk.job_description.id not in unique_jds:
-                unique_jds[chunk.job_description.id] = {
-                    "job_description": chunk.job_description,
-                    "chunks": [chunk],
-                }
-            else:
-                unique_jds[chunk.job_description.id]["chunks"].append(chunk)
 
-        results = []
-        for k, v in unique_jds.items():
-            score = sum([c.distance for c in v["chunks"]]) / len(v["chunks"])
-            job_description = v["job_description"]
-            results.append(JobDescriptionSearchResult(score, job_description, v["chunks"]))
-
-        return sorted(results, key=lambda r: r.score)
 
 
 class JobDescriptionChunk(AbstractBaseModel):
@@ -232,14 +224,51 @@ class JobDescriptionChunk(AbstractBaseModel):
     embedding = VectorField(dimensions=384)
 
     def __str__(self):
-        return f"{self.job_description.title} - {self.chunk[:50]}"
+        return f"{self.job_description.title}  - {self.chunk[:50]}"
 
-
-class JobDescriptionSearchResult:
-    def __init__(self, score, job_description, chunks):
-        self.score = score
-        self.job_description = job_description
-        self.chunks = chunks
+class Bible(AbstractBaseModel):
+    citation = models.CharField(max_length=255)
+    book = models.CharField(max_length=255)
+    chapter = models.IntegerField(default=0)
+    verse = models.IntegerField(default=0)
+    token_count = models.IntegerField(null=True)
+    text = models.TextField()
+    embedding = VectorField(dimensions=384)
 
     def __str__(self):
-        return f"{self.score}: {self.job_description.title}"
+        return f"{self.book} {self.chapter}:{self.verse} {self.text[:300]}"
+
+    @classmethod
+    def import_bible_from_csv(cls):
+
+        bible_csv_file = os.path.join(settings.BASE_DIR, '..', '..', 'data')
+        csv_path = '/home/pranay/Code/embeddings-search-demo/data/bible_modified.csv'
+        bible_collector = []
+        with open(csv_path, 'r') as f:
+            bible_csvreader = csv.DictReader(f, delimiter=',')
+            start_time = time.time()
+            for row in bible_csvreader:
+                print(row)
+                bible_collector.append(
+                    cls(
+                        citation=row['citation'],
+                        book=row['book'],
+                        chapter=row['chapter'],
+                        verse=row['verse'],
+                        text=row["text"]
+                    )
+                )
+        
+        cls.objects.bulk_create(bible_collector)
+        print(f'Loaded in {time.time() - start_time} seconds.')
+
+    def generate_embeddings(self):
+        model = SentenceTransformer("all-MiniLM-L6-v2")
+        tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2")
+
+        # Create a tuple containing text, tokens and embeddings
+        txt_tkns_embeds = (self.text, tokenizer.tokenize(self.text),  model.encode(self.text))
+
+        
+
+
